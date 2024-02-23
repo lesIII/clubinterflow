cc84a504b1a0671306e267d3faec635109cb9eb0
leshii <160965388+leshii@users.noreply.github.com>
2024-02-23 13:02:57 +0100

    Update page.js
    flowchart dimension change to smaller

--- app/workflow/page.js
+++ app/workflow/page.js
@@ -8,7 +8,7 @@ export default function WorkflowPage() {
 	return (
 
 
-		<div className="flex flex-col space-y-4" style={{width: '100vw', height: '100vh'}}>
+		<div className="flex flex-col space-y-4" style={{width: '80vw', height: '90vh'}}>
 			<h1 className={`${title()} `}>Workflow</h1><br></br>
 			<Flow/>
 		</div>
